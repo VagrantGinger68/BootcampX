70c84e09815f163716a1997ce90e9daa0b3deb0d
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-09-18 15:58:41 -0400

    Joining tables practice

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -14,3 +14,9 @@ CREATE TABLE  assignment_submissions(
   duration INTEGER,
   submission_date DATE
 );
+
+SELECT students.name, cohorts.name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM students 
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
